a3584136a5a0eb4090b128e2ca9d4204177aff37
brenopelegrin <breno.pelegrin@usp.br>
2022-12-11 21:01:19 -0300

    change gunicorn loglevel to info

--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -2,6 +2,6 @@ import os
 bind = '[::]:8080'
 #accesslog = 'gunicorn.log'
 #errorlog = 'gunicorn.error.log'
-loglevel='debug'
+loglevel='info'
 workers = int(os.getenv('GUNICORN_WORKERS'))
 threads = int(os.getenv('GUNICORN_THREADS'))
