5ef5dc223a7692a810d3fcba7350a77c8f522d29
brenopelegrin <breno.pelegrin@usp.br>
2022-12-18 21:00:46 -0300

    Change /task/<>/view argtype to string to support Celery uuids

--- api/app.py
+++ api/app.py
@@ -3,12 +3,12 @@ from server import app, api
 from resources.web import NewTask, ViewTask, Ping
 
 api.add_resource(NewTask, '/task/new')
-api.add_resource(ViewTask, '/task/<int:task_id>/view')
+api.add_resource(ViewTask, '/task/<string:task_id>/view')
 api.add_resource(Ping, '/ping')
 
 @app.route('/')
 def documentation():
-    return "You can view the server-end documentation on <a href='https://github.com/brenopelegrin/flask-tasks/'>GitHub.</a>"
+    return "You can view the server-end documentation on <a href='https://github.com/brenopelegrin/flask-tasks-docker/'>GitHub.</a>"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port='5000')
