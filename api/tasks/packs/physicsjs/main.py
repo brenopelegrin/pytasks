03c6209dc79ef058cbe3c04fae7facfcbd67a86b
brenopelegrin <breno.pelegrin@usp.br>
2023-04-04 19:17:11 -0300

    bugfix/taskpacks/physicsjs: corrected resources import to tasks.packs.physicsjs.resources. Modified .gitignore to ignore installed.lock files

--- api/tasks/packs/physicsjs/main.py
+++ api/tasks/packs/physicsjs/main.py
@@ -1,7 +1,7 @@
 def init(celery_app, **global_decorators):
     @celery_app.task
     def mov3d(dt: float, r0: list, v0: list, mass: float, radius: float, drag: bool):
-        from resources import mov3d as mov3d_module
+        from tasks.packs.physicsjs.resources import mov3d as mov3d_module
         body = mov3d_module.SphericalBody(mass=mass, radius=radius, drag_coefficient=0.5)
         fluid = mov3d_module.Fluid(density=1.184, knematic_viscosity=15.52e-6)
 
